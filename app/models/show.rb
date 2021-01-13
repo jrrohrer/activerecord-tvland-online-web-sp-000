f117277bb3f3161aefc32cd0f29a9d61d5a92e72
Jessica Rohrer <jessie.rohrer@gmail.com>
2021-01-13 02:17:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,6 @@ class Show < ActiveRecord::Base
   belongs_to :network
   
   def actors_list
-    self.actors.map {|actors| actors.fullname}
-      
+    self.actors.map {|actors| actors.full_name}
   end
 end
